ed95688af499983f4379f9c24f6fdc0a469d04c3
Mohammad Moattar <moattarwork@msn.com>
2021-07-23 10:03:56 +0100

    Add few test for the key vault configuration

--- src/Easify.Azure.AspNetCore/KeyVaults/KeyVaultExtensions.cs
+++ src/Easify.Azure.AspNetCore/KeyVaults/KeyVaultExtensions.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 using System.Security.Cryptography.X509Certificates;
 using Azure.Extensions.AspNetCore.Configuration.Secrets;
@@ -9,18 +10,21 @@ namespace Easify.Azure.AspNetCore.KeyVaults
 {
     public static class KeyVaultExtensions
     {
-        public static IConfigurationBuilder AddAzureKeyVault(this IConfigurationBuilder builder)
+        [ExcludeFromCodeCoverage]
+        public static IConfigurationBuilder AddAzureKeyVault(this IConfigurationBuilder builder, Action<KeyVaultOptions> configure = null)
         {
             if (builder == null) throw new ArgumentNullException(nameof(builder));
             
             var config = builder.Build();
             var options = new KeyVaultOptions();
             config.GetSection(nameof(KeyVaultOptions)).Bind(options);
+            
+            configure?.Invoke(options);
 
             if (!options.KeyVaultNames.Any())
                 return builder;
             
-            using var store = new X509Store(StoreLocation.LocalMachine);
+            using var store = new X509Store(options.LocalCertificateStore);
             store.Open(OpenFlags.ReadOnly);
             var certs = store.Certificates.Find(
                 X509FindType.FindByThumbprint,
@@ -37,9 +41,10 @@ namespace Easify.Azure.AspNetCore.KeyVaults
             return builder;
         }
 
-        public static IConfigurationBuilder AddAzureKeyVault(this ConfigurationBuilder builder, Func<bool> condition)
+        public static IConfigurationBuilder AddAzureKeyVault(this ConfigurationBuilder builder, Func<bool> condition,
+            Action<KeyVaultOptions> configure = null)
         {
-            return condition() ? builder.AddAzureKeyVault() : builder;
+            return condition() ? builder.AddAzureKeyVault(configure) : builder;
         }
     }
 }
--- src/Easify.Azure.AspNetCore/KeyVaults/KeyVaultOptions.cs
+++ src/Easify.Azure.AspNetCore/KeyVaults/KeyVaultOptions.cs
@@ -1,10 +1,13 @@
-﻿namespace Easify.Azure.AspNetCore.KeyVaults
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace Easify.Azure.AspNetCore.KeyVaults
 {
     public class KeyVaultOptions
     {
         public string AzureAdTenantId { get; set; }
         public string AzureAdApplicationId { get; set; }
         public string AzureAdApplicationCertThumbprint { get; set; }
+        public StoreLocation LocalCertificateStore { get; set; } = StoreLocation.CurrentUser;
         public string[] KeyVaultNames { get; set; } = { };
     }
 }
