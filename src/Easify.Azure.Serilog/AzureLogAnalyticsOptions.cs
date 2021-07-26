d460163951976d9f692551b82a4787c2b8d4351d
Mohammad Moattar <moattarwork@msn.com>
2021-07-26 16:11:05 +0100

    Initial support for Azure logs

---
+++ src/Easify.Azure.Serilog/AzureLogAnalyticsOptions.cs
@@ -0,0 +1,11 @@
+﻿namespace Easify.Azure.Serilog
+{
+    public sealed class AzureLogAnalyticsOptions
+    {
+        public string WorkspaceId { get; set; }
+        public string AuthenticationId { get; set; }
+        public string LogName { get; set; }
+        public int? LogBufferSize { get; set; }
+        public int? BatchSize { get; set; }
+    }
+}
---
+++ src/Easify.Azure.Serilog/SinkBuilderContext.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using Easify.Logging.SeriLog;
+using Microsoft.Extensions.Configuration;
+using Serilog;
+
+namespace Easify.Azure.Serilog
+{
+    [ExcludeFromCodeCoverage]
+    public static class SinkBuilderContext
+    {
+        public static ISinkBuilderContext UseAzureTableStorage(this ISinkBuilderContext sinkBuilderContext, IConfiguration configuration, Action<AzureTableStorageOptions> configure = null)
+        {
+            if (configuration == null) throw new ArgumentNullException(nameof(configuration));
+            
+            var options = new AzureTableStorageOptions();
+            configuration.GetSection(nameof(AzureTableStorageOptions)).Bind(options);
+            configure?.Invoke(options);
+
+            if (string.IsNullOrWhiteSpace(options.ConnectionString))
+                throw new ArgumentException($"{nameof(options.ConnectionString)} is missing in AzureTableStorageOptions");
+            
+            var loggerConfiguration = sinkBuilderContext.LoggerConfiguration.WriteTo.AzureTableStorage(options.ConnectionString);
+
+            return sinkBuilderContext.Clone(loggerConfiguration);
+        }
+        
+        public static ISinkBuilderContext UseAzureLogAnalytics(this ISinkBuilderContext sinkBuilderContext, IConfiguration configuration, Action<AzureLogAnalyticsOptions> configure = null)
+        {
+            if (configuration == null) throw new ArgumentNullException(nameof(configuration));
+            
+            var options = new AzureLogAnalyticsOptions();
+            configuration.GetSection(nameof(AzureLogAnalyticsOptions)).Bind(options);
+            configure?.Invoke(options);
+
+            if (string.IsNullOrWhiteSpace(options.WorkspaceId))
+                throw new ArgumentException($"{nameof(options.WorkspaceId)} is missing in AzureLogAnalyticsOptions");
+            
+            if (string.IsNullOrWhiteSpace(options.AuthenticationId))
+                throw new ArgumentException($"{nameof(options.AuthenticationId)} is missing in AzureLogAnalyticsOptions");
+            
+            var loggerConfiguration = sinkBuilderContext
+                .LoggerConfiguration
+                .WriteTo
+                .AzureAnalytics(options.WorkspaceId,options.AuthenticationId);
+
+            return sinkBuilderContext.Clone(loggerConfiguration);
+        }
+    }
+}
